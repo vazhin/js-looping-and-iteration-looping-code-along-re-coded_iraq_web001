b08dbdcda8ecfbff9fede1c441e81902f0952fc1
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-12 19:35:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const names = ["Ada", "Brendan", "Ali"];
 function writeCards(names, event){
   for (let i = 0; i < names.length; i++){
     if (i = 0){
-      const massages = [`Thank you, ${name[i]}, for the wonderful ${event} gift!`];
+      const massages = [`Thank you, ${names[i]}, for the wonderful ${event} gift!`];
     }
     else {
-      massages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+      massages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
   }
   return massages;
