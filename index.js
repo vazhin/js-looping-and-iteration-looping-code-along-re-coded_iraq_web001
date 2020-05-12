be06355669270a0975830e9c1db8b1ffe4ad4d8b
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-12 19:55:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let massages = [];
+
+function writeCards(names, event){
+  for (let i = 0; i < names.length; i++){
+    massages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return massages;
+}
+
+function countDown (positiveInt){
+  let i = positiveInt;
+  while ( i >= 0)
+  console.log(i--);
+}
