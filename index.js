879f7095c495a42e504b60ba1e89880f59a71d9f
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-12 19:29:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+
+function writeCards(name, event){
+  for (let i = 0; i < names.length; i++){
+    if (i = 0){
+      const massages = [`Thank you, ${name[i]}, for the wonderful ${event} gift!`)]
+    }
+    else {
+      massages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)])
+    }
+  }
+  console.log(massages);
+}
+
+
+
+
+
+
+
+writeCards(names, "birthday");
