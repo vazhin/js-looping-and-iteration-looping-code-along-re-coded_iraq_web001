ff05664f58abbc7cea47547901cf586fffa15e58
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-12 19:47:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Code your solutions in this file
+let massages = [];
+
 function writeCards(names, event){
   for (let i = 0; i < names.length; i++){
-    const massages = [`Thank you, ${names[i]}, for the wonderful ${event} gift!`];
-    if (i > 0){
-      massages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-    }
+    massages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
   return massages;
 }
